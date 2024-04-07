05a32747444c7ca3cee2c7c49cf5ccc06e94b957
Prosper Arineitwe Asiimwe <prox0591@gmail.com>
2024-04-07 12:29:04 +0200

    cors policy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const indexRouter = require("./src/routes");
 
@@ -15,6 +17,9 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
